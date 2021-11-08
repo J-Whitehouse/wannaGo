dcfdb1294ad0c4180f9e7228ac449e3a5c448541
Jarret <jarrwhit@iu.edu>
2021-11-08 14:53:22 -0500

    added FireBase and plist stuff

--- WannaGo/AppDelegate.swift
+++ WannaGo/AppDelegate.swift
@@ -6,16 +6,18 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+            // Override point for customization after application launch.
+            FirebaseApp.configure()
+            return true
     }
+
 
     // MARK: UISceneSession Lifecycle
 
@@ -31,6 +33,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
