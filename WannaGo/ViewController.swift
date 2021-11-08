fdc67669566a27b249dfadafff6fea4033b59a59
Jarret <jarrwhit@iu.edu>
2021-11-08 13:41:15 -0500

    Initial Commit

---
+++ WannaGo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WannaGo
+//
+//  Created by Whitehouse, Jarret M on 11/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
